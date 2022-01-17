d7d41f140e7504719dc96ed75d9ca9ba3c3cdb9e
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-17 23:49:25 +0300

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Владимир Рябов on 17.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
